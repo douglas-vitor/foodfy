2b7463977fe2e8f5d5707e22e5e8da405204fcc1
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-06-27 09:02:49 -0300

    Add funcionalidade de visible/hidden detalhes receita

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,3 +6,36 @@ for (let card of cards) {
     window.location.href = `/recipe?id=${infoId}`
     })
 }
+
+document.querySelector(".detalhes").addEventListener("click", function () {
+    let verify = document.querySelector(".oculto").classList.contains("view-div")
+    if (verify == false) {
+        document.querySelector(".oculto").classList.add("view-div")
+        document.getElementById("ingredientes").innerHTML = "ESCONDER"
+    } else {
+        document.querySelector(".oculto").classList.remove("view-div")
+        document.getElementById("ingredientes").innerHTML = "MOSTRAR"
+    }
+})
+
+document.querySelector(".detalhes1").addEventListener("click", function () {
+    let verify = document.querySelector(".oculto1").classList.contains("view-div")
+    if (verify == false) {
+        document.querySelector(".oculto1").classList.add("view-div")
+        document.getElementById("preparo").innerHTML = "ESCONDER"
+    } else {
+        document.querySelector(".oculto1").classList.remove("view-div")
+        document.getElementById("preparo").innerHTML = "MOSTRAR"
+    }
+})
+
+document.querySelector(".detalhes2").addEventListener("click", function () {
+    let verify = document.querySelector(".oculto2").classList.contains("view-div")
+    if (verify == false) {
+        document.querySelector(".oculto2").classList.add("view-div")
+        document.getElementById("informacoes").innerHTML = "ESCONDER"
+    } else {
+        document.querySelector(".oculto2").classList.remove("view-div")
+        document.getElementById("informacoes").innerHTML = "MOSTRAR"
+    }
+})
