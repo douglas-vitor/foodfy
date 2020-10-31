8c20bed27c7712db892584636e518efc9942a359
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-10-30 22:48:03 -0300

    Update sql

---
+++ src/config/database.sql
@@ -0,0 +1,52 @@
+CREATE TABLE "files" (
+	"id" SERIAL PRIMARY KEY,
+  "name" text,
+  "path" text NOT NULL
+);
+
+CREATE TABLE "recipe_files" (
+	"id" SERIAL PRIMARY KEY,
+  "recipe_id" int,
+  "file_id" int
+);
+
+CREATE TABLE "chefs" (
+	"id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "file_id" int
+);
+
+CREATE TABLE "recipes" (
+    "id" SERIAL PRIMARY KEY,
+    "chef_id" int NOT NULL,
+    "title" text NOT NULL,
+    "ingredients" text[] NOT NULL,
+    "preparation" text[] NOT NULL,
+    "information" text NOT NULL,
+    "created_at" timestamp DEFAULT (now()),
+    "updated_at" timestamp DEFAULT (now())
+);
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+
+ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "reset_token" text,
+  "reset_token_expires" text,
+  "is_admin" BOOLEAN DEFAULT false,
+  "created_at" TIMESTAMP DEFAULT(now()),
+  "updated_at" TIMESTAMP DEFAULT(now())
+);
+
+--add column user_id in recipes table
+ALTER TABLE "recipes" ADD COLUMN user_id int;
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
