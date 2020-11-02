cd799e9a78d691257f61fefb4bd85e1faee941d3
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-11-02 10:40:27 -0300

    Update route create chef

--- src/routes/chefs.js
+++ src/routes/chefs.js
@@ -7,7 +7,7 @@ const chefsController = require("../app/controllers/chefsController")
 
 // Admin chefs
 routes.get("/admin/chefs", chefsController.chefs)
-routes.get("/admin/chefs/create_chef", chefsController.createChef)
+routes.get("/admin/chefs/create", chefsController.createChef)
 routes.post("/admin/chefs", multer.array("photos", 1), chefsController.postChef)
 routes.get("/admin/chefs/:id", chefsController.showChef)
 routes.get("/admin/chefs/:id/edit", chefsController.editChef)
